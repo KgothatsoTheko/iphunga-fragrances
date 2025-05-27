d79fc18a507c81d415093c371611c0ec629ee362
KgothatsoTheko <kgothatsotheko7@gmail.com>
2025-05-27 21:45:05 +0200

    admin updates pt2

--- iphunga-fragrances/src/app/pages/admin/add-products/add-products.component.ts
+++ iphunga-fragrances/src/app/pages/admin/add-products/add-products.component.ts
@@ -16,10 +16,17 @@ import { ApiService } from '../../../services/api.service';
 })
 export class AddProductsComponent {
 
+  fileName:any | string="No Image"
+  upload:any
+  fileChoosen:any
+  uploadForm: FormGroup
+
   adminForm = new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl('', [Validators.required]),
-    category: new FormControl('', Validators.required)
+    category: new FormControl('', Validators.required),
+    volume: new FormControl('', Validators.required)
+     
   })
 
   category: any[] = [
@@ -27,8 +34,44 @@ export class AddProductsComponent {
     {value: 'Female', viewValue: 'Female'},
   ]
 
+  valume: any[] = [
+    {value: '50ml', viewValue: '100ml'},
+    {value: '50ml', viewValue: '100ml'},
+  ]
+
   constructor(private dialog:MatDialogRef<AddProductsComponent>, private location: Location, private api: ApiService, private router: Router, private snackbar: MatSnackBar) {
 
+    this.uploadForm = new FormGroup({
+      file: new FormControl('')
+  })
+  }
+
+  uploadFile(){
+    this.upload = document.getElementById('upload') as HTMLInputElement;
+    this.fileChoosen = document.getElementById('file-choosen');
+    if (this.upload && this.fileChoosen) {
+      this.upload.addEventListener('change', (event: Event) => {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files.length > 0) {
+          this.fileChoosen.textContent = input.files[0].name;
+          this.fileName = input.files[0].name
+      
+          const formData = new FormData();
+          formData.append('file', input.files[0], input.files[0].name)
+          console.log(formData);
+          // this.api.genericPost(`upload2/${addedMentor.fullName}`, formData).subscribe(
+          //   (resposnse:any)=> {
+          //     this.snackbar.open(`Success: ${resposnse}`, 'Ok', {duration: 3000})
+          //     console.log("res", resposnse);
+          //     this.dialog.close(true)
+          //   },
+          //   (error:any)=> {
+          //     return this.snackbar.open(`Error: ${error}`, 'Ok', {duration: 3000})
+          //   }
+          // )
+        }
+      }
+    )}
   }
 
   login() {
