b745661b473b564e3c3b989d5056ee308ec625f7
KgothatsoTheko <kgothatsotheko7@gmail.com>
2025-05-31 02:39:02 +0200

    admin updates pt5

--- iphunga-fragrances/src/app/pages/product-list/product-list.component.ts
+++ iphunga-fragrances/src/app/pages/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MaterialModule } from '../../modules/material/material.module';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -12,7 +14,24 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductListComponent {
 
-  constructor(private router: Router){}
+  allProducts: any
+
+  constructor(private router: Router, private api: ApiService, private snackbar: MatSnackBar){
+    this.loadProducts()
+  }
+
+  private loadProducts(){
+      this.api.genericGet(`get-products`).subscribe(
+            (resposnse:any)=> {
+              this.allProducts = resposnse
+              this.snackbar.open(`Success: ${resposnse}`, 'Ok', {duration: 3000})
+              console.log("res", this.allProducts);
+            },
+            (error:any)=> {
+              return this.snackbar.open(`Error: ${error}`, 'Ok', {duration: 3000})
+            }
+          )
+    }
 
   goToProduct(){
     this.router.navigate(['/landing/product-detail'])
